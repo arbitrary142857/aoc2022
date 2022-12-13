c3c7bd790f15b1e5923440d84d07032946595bd4
arbitrary142857 <120224492+arbitrary142857@users.noreply.github.com>
2022-12-13 16:53:47 -0500

    Create day07part1.py

---
+++ day07/day07part1.py
@@ -0,0 +1,48 @@
+import sys
+
+sys.stdin = open("input07.txt", "r")
+sys.stdout = open("output07.txt", "w")
+
+dictDirectories = {}
+
+path = [] # shows current /a/b/c/etc. path
+
+for i in range(1109):
+  
+  information = input().split(" ")
+
+  # "$ cd" CASE
+  if information[0] == "$" and information[1] == "cd":
+
+    # renaming to avoid duplicate ambiguity
+    if information[2] in dictDirectories.keys():
+      information[2] = information[2] + str(i) 
+
+    if information[2] == "..":
+      path = path[:-1]
+      continue
+
+    path.append(information[2])
+
+    dictDirectories[information[2]] = 0
+    continue
+
+  # "$ ls" CASE
+  if information[0] == "$" and information[1] == "ls":
+    continue
+
+  # "dir" CASE
+  if information[0] == "dir":
+    continue
+
+  # "[num] file" CASE
+  for directory in path:
+    dictDirectories[directory] += int(information[0])
+
+total = 0
+
+for directory in dictDirectories:
+  if dictDirectories[directory] <= 100000:
+  total += dictDirectories[directory]
+
+print(total)
